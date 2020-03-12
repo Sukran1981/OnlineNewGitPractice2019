ea0a84866b816a23f679fd1703af5a2475a31846
Sukran1981 <58491627+Sukran1981@users.noreply.github.com>
2020-03-12 12:07:02 -0400

    Update GoogleTests.java
    sukran femae 39

--- src/test/java/tests/GoogleTests.java
+++ src/test/java/tests/GoogleTests.java
@@ -3,6 +3,9 @@ package tests;
 public class GoogleTests {
 
     public static void main(String[] args) {
+        String str="helllo word";
         System.out.println("hello world");
+        
+        
     }
 }
