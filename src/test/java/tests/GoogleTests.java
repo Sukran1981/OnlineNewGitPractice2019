c494f2de73182134fde671b18bf5019d1ec557b3
First name Last name <58491627+Sukran1981@users.noreply.github.com>
2020-04-08 19:13:38 -0400

    first commit

--- src/test/java/tests/GoogleTests.java
+++ src/test/java/tests/GoogleTests.java
@@ -2,14 +2,7 @@ package tests;
 
 public class GoogleTests {
 
-    public static void main(String[] args) {
-<<<<<<< HEAD
 
-=======
-        String str="helllo word";
->>>>>>> 5d9d5c1b000b04daef5ffd10dfc9be104c5c0dfa
-        System.out.println("hello world");
-        
         
     }
-}
+g
